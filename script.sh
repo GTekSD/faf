be99ee058cdfc7d894d1a8fa8c97683697c7b34f
Сухас Дхолз <55411358+GTekSD@users.noreply.github.com>
2023-01-27 19:16:45 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Output Apache version
+echo "Apache version:"
+apache2 -v
+echo
+
+# Output MySQL version
+echo "MySQL version:"
+mysql --version
+echo
+
+# Output PHP version
+echo "PHP version:"
+php -v
