6c081a68e090cfaf82d129f78b1f16f64d472969
Сухас Дхолз <55411358+GTekSD@users.noreply.github.com>
2023-09-06 13:05:31 +0530

    Create IRCMDUF.sh

---
+++ IRCMDUF.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Function to display usage information
+usage() {
+    echo "Usage: $0 <command> <filename>"
+    echo "  -h, --help    Display this help message."
+    exit 1
+}
+
+# Check if the help argument is provided
+if [ "$#" -eq 1 ] && { [ "$1" = "-h" ] || [ "$1" = "--help" ]; }; then
+    usage
+elif [ "$#" -ne 2 ]; then
+    echo "Error: Invalid number of arguments."
+    usage
+fi
+
+command="$1"
+filename="$2"
+
+if [ ! -f "$filename" ]; then
+    echo "Error: File '$filename' not found."
+    exit 1
+fi
+
+while IFS= read -r line; do
+    full_command="$command $line"
+    echo "Executing: $full_command"
+    eval "$full_command"
+done < "$filename"
